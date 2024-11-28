9a0dc2ea30984e2b683a393548a18a6e93565a09
Haruta Tanaka <haruubusiness@gmail.com>
2024-11-29 01:58:09 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './Hero.jsx'
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen overflow-x-hidden">
+      <Hero />
+    </main>
+  )
+}
+
+export default App
