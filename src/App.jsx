f7dc28a1fc8c1f88eeea635fcea5a6581a420d14
Haruta Tanaka <haruubusiness@gmail.com>
2024-12-09 18:01:22 +0530

    added music

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   return (
-    <main className="relative min-h-screen overflow-x-hidden">
+    <main className="relative min-h-screen overflow-x-hidden ">
       <Navbar/>
       <Hero />
       <About/>
