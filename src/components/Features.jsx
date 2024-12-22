00b0f99f3d0e33e846dab120e59df8a365fcaad2
Haruta Tanaka <haruubusinesss@gmail.com>
2024-12-23 00:01:18 +0530

    story

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -5,11 +5,25 @@ const BentoTilt = ({ children, className = '' }) => {
   const [transformStyle, setTransformStyle] = useState('');
   const itemRef = useRef(null);
 
-  const handleMouseMove = () => {
-    // Logic for the mouse move, if needed
-  };
+  const handleMouseMove = (e) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } = itemRef.current.getBoundingClientRect();
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 5; 
+    const tiltY = (relativeX - 0.5) * -5;  
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) 
+    rotateY(${tiltY}deg) scale3d(0.95, 0.98, 0.98)`;
+    setTransformStyle(newTransform); 
+};
 
   const handleMouseLeave = () => {
+    
+
     setTransformStyle('');
   };
 
